7c74156d65d8789e567492ef8e193221f6ca0a3f
Alberto Caio <albertocaiobr@hotmail.com>
2026-01-29 00:02:51 -0300

    Primeiro teste com rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/" : "Curso de Node.Js",
+    "/livros" : "Entrando na rota de Livros",
+    "/autores" : "Entrando na rota de autores"
+};
+
 const server = http.createServer((req, res)=>{
     res.writeHead(200, { "Content-type": "text/plain"});
-    res.end("Curso de Node!");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT,()=>{
