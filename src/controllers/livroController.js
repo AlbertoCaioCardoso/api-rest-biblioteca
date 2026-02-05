43264c7dc0fa0a3e0c108ee94f3dd2f2b8fc4af2
Alberto Cardoso <albertocaiobr@hotmail.com>
2026-02-05 15:43:39 -0300

    Refatoração parcial de rotas, criação de controller e refatoração da model

---
+++ src/controllers/livroController.js
@@ -0,0 +1,49 @@
+import livro from '../models/Livro.js';
+
+class LivroController {
+
+    //GET
+    static async listarLivros(req, res){
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (error) {
+            res.status(500).json({message: `${error.message} - FALHA AO LISTAR LIVROS`})
+        }
+    };
+
+    //GET:ID
+    static async listarLivroById(req, res){
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (error) {
+            res.status(500).json({message: `${error.message} - FALHA AO BUSCAR LIVRO`})
+        }
+    };
+
+    //PUT
+    static async atualizarLivroById(req, res){
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message : "LIVRO ATUALIZADO"});
+        } catch (error) {
+            res.status(500).json({message: `${error.message} - FALHA AO ATUALIZAR LIVRO`})
+        }
+    };
+
+    //POST
+    static async cadastrarLivros(req, res){
+        try{
+            const novoLivro = await livro.create(res.body);
+        res.status(201).json(novoLivro);
+        } catch(error){
+            res.status(500).json({message: `${error.message} - FALHA AO CADASTRAR LIVRO`});
+        };
+    };
+
+};
+
+export default LivroController;
---
+++ src/routes/livrosRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import LivroController from '../controllers/livroController.js';
+
+const routes = express.Router();
+
+routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.listarLivroById);
+routes.post("/livros", LivroController.cadastrarLivros);
+routes.put("/livros/:id", LivroController.atualizarLivroById);
+
+export default routes;
